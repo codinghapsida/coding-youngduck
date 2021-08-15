4c84400736a885959b81de3ab74b218c386cd5ea
youngduck <dudejr5839@naver.com>
2021-08-15 19:31:13 +0900

    1주차

--- week1/1417.py
+++ week1/1417.py
@@ -1,1 +1,22 @@
 #국회의원 선거
+
+import sys
+
+n=int(sys.stdin.readline())
+num=int(sys.stdin.readline())
+data=[]
+cnt=0
+
+#n=1일때 생각해주어야함.
+if n ==1:
+    cnt=0
+else:
+    for i in range(n-1):
+        data.append(int(sys.stdin.readline()))
+    while num <= max(data):
+        data[data.index(max(data))]-=1
+        num+=1
+        cnt+=1
+
+
+print(cnt)
