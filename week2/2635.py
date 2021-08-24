b576844d7341b78fa79345ddee82f9bbd8810f86
youngduck <dudejr5839@naver.com>
2021-08-24 19:04:09 +0900

    2주차

---
+++ week2/2635.py
@@ -0,0 +1,16 @@
+# a b a-b 2b-a 2a-3b 5b-3a .....
+# import sys
+# import heapq
+
+# max_data=[]
+# N=sys.stdin.readline()
+
+
+# for i in range(N):
+#     if i >=0:
+#         data.append(i)
+
+# if len(max_data)>len(data):
+#     max_data=max
+
+# prin(*max_data)
