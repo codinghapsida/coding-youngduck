8f59fd2999b3d120f9627f92661243b5f56efcd4
youngduck <dudejr5839@naver.com>
2021-09-09 23:30:29 +0900

    4주차

--- week4/7513.py
+++ week4/7513.py
@@ -6,6 +6,7 @@ for t in range(int(sys.stdin.readline())):
     for m in range(int(sys.stdin.readline())):
         data.append(sys.stdin.readline().rstrip())
     # password=[print(data[k],end='') for n in range(int(sys.stdin.readline())) for k in list(map(int,sys.stdin.readline().split()))[1:]]
+    # print시 마지막에만 개행문자 추가하는 조건 넣어줘야되서 리스트내포 탈락.
     for n in range(int(sys.stdin.readline())):
         key=list(map(int,sys.stdin.readline().split()))
         for k in key[1:]:
